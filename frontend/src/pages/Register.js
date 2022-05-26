ffe2cea0b586f44e0592957a06fdaf60b34eb53b
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2022-05-26 16:46:02 +0200

    add simple form

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,4 +1,8 @@
 import React, { useEffect } from "react";
+import { TextInput, Paper, Title, Container, Group, Button, Select } from '@mantine/core';
+import { DatePicker } from '@mantine/dates';
+import { useForm } from '@mantine/form';
+import { Calendar } from 'tabler-icons-react';
 
 export const Register = () => {
 
@@ -6,9 +10,61 @@ export const Register = () => {
     document.title = "Bürgerbüro - Melden";
   }, []);
 
+  const form = useForm({
+    initialValues: {
+      firstname: '', lastname: '', email: '', birthname: '', birthdate: '', place_of_birth: '',
+      gender: '', street: '', housenumber: '', city_code: '', city: ''
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+    },
+  });
+
   return (
     <div>
-      <h1>Melden</h1>
-    </div>
+      <Container size={1000} my={40}>
+        <Title
+          align="center"
+          sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 900 })} >
+          In der SmartCity melden.
+        </Title>
+
+        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+          <form onSubmit={form.onSubmit((values) => console.log(values))}>
+            <Group spacing="xs" grow>
+              <TextInput label="Vorname" placeholder="Max" required mt="md" {...form.getInputProps('firstname')} />
+              <TextInput label="Nachname" placeholder="Mustermann" required mt="md" {...form.getInputProps('lastname')} />
+              <Select label="Geschlecht" placeholder="Bitte auswählen" required mt="md" {...form.getInputProps('gender')}
+                data={[
+                  { value: 'm', label: 'Männlich' },
+                  { value: 'w', label: 'Weiblich' },
+                  { value: 'd', label: 'Divers' }
+                ]}
+              />
+            </Group>
+            <Group spacing="xs" grow>
+              <DatePicker icon={<Calendar size={16} />} placeholder="Bitte auswählen" label="Geburtsdatum" mt="md" required {...form.getInputProps('birthdate')} />
+              <TextInput label="Geburtsort" placeholder="" mt="md" {...form.getInputProps('place_of_birth')} />
+              <TextInput label="Geburtsname" placeholder="" mt="md" {...form.getInputProps('birthname')} />
+            </Group>
+            <Group spacing="xs" grow>
+              <TextInput label="E-Mail" type="email" placeholder="max@mustermann.de" required mt="md" {...form.getInputProps('email')} />
+            </Group>
+            <Group spacing="xs" grow>
+              <TextInput label="Straße" placeholder="Musterweg" required mt="md" {...form.getInputProps('street')} />
+              <TextInput label="Hausnummer" placeholder="1a" required mt="md" {...form.getInputProps('housenumber')} />
+            </Group>
+            <Group spacing="xs" grow>
+              <TextInput label="Postleitzahl" type="number" placeholder="12345" required mt="md" {...form.getInputProps('city_code')} />
+              <TextInput label="Ort" placeholder="Musterhausen" required mt="md" {...form.getInputProps('city')} />
+            </Group>
+            <Button fullWidth mt="xl" type="submit">
+              Jetzt melden
+            </Button>
+          </form>
+        </Paper>
+      </Container >
+    </div >
   );
 };
