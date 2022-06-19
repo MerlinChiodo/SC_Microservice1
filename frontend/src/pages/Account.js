794e108420f42a02d7fae71b51c9c4d68f295b0e
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2022-06-19 13:16:46 +0200

    create basic page layout

--- frontend/src/pages/Account.js
+++ frontend/src/pages/Account.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from "react";
+import { PageContainer } from "../components/PageContainer";
+import { Grid, Text, Button } from "@mantine/core";
 
 export const Account = () => {
 
@@ -7,8 +9,36 @@ export const Account = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Meine Daten</h1>
-    </div>
+    <PageContainer title="Meine Daten" size={1000}>
+      <Grid gutter="lg">
+        <Grid.Col span={12}>
+          <Text size="lg" weight={800}>Name</Text>
+          <Text>Max Mustermann (geb. Muster)</Text>
+        </Grid.Col>
+        <Grid.Col span={12} xs={6}>
+          <Text size="lg" weight={800}>Geburtsdatum</Text>
+          <Text>01.01.2000</Text>
+        </Grid.Col>
+        <Grid.Col span={12} xs={6}>
+          <Text size="lg" weight={800}>Geburtsort</Text>
+          <Text>Musterstadt</Text>
+        </Grid.Col>
+        <Grid.Col span={12} xs={6}>
+          <Text size="lg" weight={800}>E-Mail</Text>
+          <Text>max@mustermann.de</Text>
+        </Grid.Col>
+        <Grid.Col span={12} xs={6}>
+          <Text size="lg" weight={800}>Adresse</Text>
+          <Text>Musterstraße 1</Text>
+          <Text>12345 Musterhausen</Text>
+        </Grid.Col>
+        <Grid.Col span={12} xs={6}>
+          <Button fullWidth color="teal">Namensänderung beantragen</Button>
+        </Grid.Col>
+        <Grid.Col span={12} xs={6}>
+          <Button fullWidth color="teal" >Umzug melden</Button>
+        </Grid.Col>
+      </Grid>
+    </PageContainer>
   );
 };
