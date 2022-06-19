1cda537637434bbeb38e31b8c5245f797f6ce452
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2022-06-19 14:46:08 +0200

    add content to modals

--- frontend/src/components/AddressChangeModal.js
+++ frontend/src/components/AddressChangeModal.js
@@ -1,10 +1,22 @@
-import { Grid, Text, Button } from "@mantine/core";
+import { Grid, TextInput, Button, NumberInput, Textarea } from "@mantine/core";
 
 export const AddressChangeModal = ({ context, id, innerProps }) => (
     <form onSubmit={(event) => innerProps.handleSubmit(event, id)}>
         <Grid grow gutter="xl" align="center">
+            <Grid.Col span={12} xs={8}>
+                <TextInput label="Straße" name="street" placeholder="Bitte eingeben" required />
+            </Grid.Col>
+            <Grid.Col span={12} xs={4}>
+                <TextInput label="Hausnummer" name="housenumber" placeholder="Bitte eingeben" required />
+            </Grid.Col>
+            <Grid.Col span={12} xs={4}>
+                <NumberInput label="Postleitzahl" name="city_ode" placeholder="Bitte eingeben" hideControls required />
+            </Grid.Col>
+            <Grid.Col span={12} xs={8}>
+                <TextInput label="Ort" name="city" placeholder="Bitte eingeben" required />
+            </Grid.Col>
             <Grid.Col span={12}>
-                <Text size="lg" weight={800}>Test</Text>
+                <Textarea label="Begründung" name="reasoning" minRows={7} placeholder="Bitte beschreibe warum die Adressänderung beantragt wird" />
             </Grid.Col>
             <Grid.Col span={12} xs={6}>
                 <Button fullWidth color="red" onClick={() => context.closeModal(id)}>Abbrechen</Button>
--- frontend/src/components/NameChangeModal.js
+++ frontend/src/components/NameChangeModal.js
@@ -1,10 +1,16 @@
-import { Grid, Text, Button } from "@mantine/core";
+import { Grid, TextInput, Button, Textarea } from "@mantine/core";
 
 export const NameChangeModal = ({ context, id, innerProps }) => (
     <form onSubmit={(event) => innerProps.handleSubmit(event, id)}>
         <Grid grow gutter="xl" align="center">
             <Grid.Col span={12}>
-                <Text size="lg" weight={800}>Test</Text>
+                <TextInput label="Vorname" name="firstname" placeholder="Bitte eingeben" required />
+            </Grid.Col>
+            <Grid.Col span={12}>
+                <TextInput label="Nachname" name="lastname" placeholder="Bitte eingeben" required />
+            </Grid.Col>
+            <Grid.Col span={12}>
+                <Textarea label="Begründung" name="reasoning" minRows={7} placeholder="Bitte beschreibe warum die Namensänderung beantragt wird" />
             </Grid.Col>
             <Grid.Col span={12} xs={6}>
                 <Button fullWidth color="red" onClick={() => context.closeModal(id)}>Abbrechen</Button>
