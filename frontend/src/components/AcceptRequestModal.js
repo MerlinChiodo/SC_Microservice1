ab55027224706487a4ded322b6e329075fc0c9d4
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2022-06-19 18:10:39 +0200

    better difference highlighting

--- frontend/src/components/AcceptRequestModal.js
+++ frontend/src/components/AcceptRequestModal.js
@@ -7,12 +7,12 @@ const getFormattedName = (request) => {
     const firstname_new = request.citizen_new.firstname;
     const lastname_new = request.citizen_new.lastname;
     let lastname, firstname;
-    if (firstname_new !== null && firstname_new.length > 0) {
+    if (firstname_new !== null && firstname_new.length > 0 && firstname_new !== firstname_old) {
         firstname = <Text color="cyan" component="span">{firstname_new}</Text>;
     } else {
         firstname = <Text component="span">{firstname_old}</Text>;
     }
-    if (lastname_new !== null && lastname_new.length > 0) {
+    if (lastname_new !== null && lastname_new.length > 0 && lastname_new !== lastname_old) {
         lastname = <Text color="cyan" component="span">{lastname_new}</Text>;
     } else {
         lastname = <Text component="span">{lastname_old}</Text>;
@@ -27,12 +27,12 @@ const getFormattedStreetAndNumber = (request) => {
     const street_new = request.citizen_new.street;
     const house_number_new = request.citizen_new.housenumber;
     let street, house_number;
-    if (street_new !== null && street_new.length > 0) {
+    if (street_new !== null && street_new.length > 0 && street_new !== street_old) {
         street = <Text color="cyan" component="span">{street_new}</Text>;
     } else {
         street = <Text component="span">{street_old}</Text>;
     }
-    if (house_number_new !== null && house_number_new.length > 0) {
+    if (house_number_new !== null && house_number_new.length > 0 && house_number_new !== house_number_old) {
         house_number = <Text color="cyan" component="span">{house_number_new}</Text>;
     } else {
         house_number = <Text component="span">{house_number_old}</Text>;
@@ -47,12 +47,12 @@ const getFormattedCityCodeAndCity = (request) => {
     const city_code_new = request.citizen_new.city_code;
     const city_new = request.citizen_new.city;
     let city_code, city;
-    if (city_code_new !== null && city_code_new.length > 0) {
+    if (city_code_new !== null && city_code_new !== city_code_old) {
         city_code = <Text color="cyan" component="span">{city_code_new}</Text>;
     } else {
         city_code = <Text component="span">{city_code_old}</Text>;
     }
-    if (city_new !== null && city_new.length > 0) {
+    if (city_new !== null && city_new.length > 0 && city_new !== city_old) {
         city = <Text color="cyan" component="span">{city_new}</Text>;
     } else {
         city = <Text component="span">{city_old}</Text>;
