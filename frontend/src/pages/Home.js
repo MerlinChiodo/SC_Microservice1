8978d454e46a9fb87dbc12b9fe91a31ae291a4ac
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2022-06-27 16:11:28 +0200

    add content to main page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,71 @@
-import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Text, Title, Anchor, List, ThemeIcon } from "@mantine/core";
+import { Link as ELink } from "tabler-icons-react";
 
 export const Home = () => {
 
+  const [permits, setPermits] = useState([]);
+
+  const loadPermits = async () => {
+    fetch("/api/permits")
+      .then(res => res.json())
+      .then(data => {
+        let permitsTemp = [];
+        data.permits.forEach(permit => {
+          permitsTemp.push(permit.title);
+        });
+        setPermits(permitsTemp);
+      })
+      .catch(error => console.error(error));
+  };
+
   useEffect(() => {
     document.title = "Bürgerbüro - Startseite";
+    loadPermits();
   }, []);
 
+  const icon = <ThemeIcon radius="xl" color="green" size={20} variant="outline" ><ELink size={15} color="gray" /></ThemeIcon>;
+
   return (
-    <div>
-      <h1>Startseite</h1>
-    </div>
+    <>
+      <Title mt={20}>Wilkommen beim Bürgerbüro</Title>
+      <Title order={2}>Das kannst du hier alles erledigen</Title>
+      <br />
+      <Text size="xl" weight={700}>alle Bürger:</Text>
+      <List size="xl" color="gray" icon={icon}>
+        <List.Item>
+          <Anchor to="/melden" component={Link} size="xl" onClick={(e) => window.scrollTo(0, 0)} color="gray">
+            in der SmartCity Melden
+          </Anchor>
+        </List.Item>
+      </List>
+      <br />
+      <Text size="xl" weight={700}>als gemeldeter Bürger der SmartCity:</Text>
+      <List size="xl" >
+        <List.Item icon={icon}>
+          <Anchor to="/genehmigungen" component={Link} size="xl" onClick={(e) => window.scrollTo(0, 0)} color="gray">
+            erteilte Genehmigungen ansehen / neue Genehmigungen beantragen
+          </Anchor>
+          {permits.length > 0 && (
+            <List withPadding listStyleType="disc" size="lg" >
+              <List.Item>
+                Mögliche Genehmigungen:
+                <List withPadding listStyleType="disc" >
+                  {permits.map(permit => (
+                    <List.Item key={permit}>{permit}</List.Item>
+                  ))}
+                </List>
+              </List.Item>
+            </List>
+          )}
+        </List.Item>
+        <List.Item icon={icon}>
+          <Anchor to="/account" component={Link} size="xl" onClick={(e) => window.scrollTo(0, 0)} color="gray">
+            Bürgerdaten ansehen/ändern
+          </Anchor>
+        </List.Item>
+      </List>
+    </>
   );
 };
